15b25997de580b13c1223b363496e44bcfdf21f2
Alex Elgin <sascha.elgin@gmail.com>
2020-01-21 22:07:10 +0600

    use promisify

--- src/server/middleware/auth-check.js
+++ src/server/middleware/auth-check.js
@@ -1,37 +1,29 @@
 import mongoose from 'mongoose';
 import { verify } from 'jsonwebtoken';
+import { promisify } from 'util';
 
 import { jwtSecret } from '../../config';
 
-const checkAuth = (req, res, next) => {
-  const unauthorized = () => res.status(401).end();
-
-  if (!req.headers.authorization) {
-    return unauthorized();
-  }
-
-  const token = req.headers.authorization.split(' ')[1];
-
-  verify(token, jwtSecret, async (err, decoded) => {
-    if (err) {
-      return unauthorized();
+const checkAuth = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      throw new Error('Unauthorized');
     }
 
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = await promisify(verify)(token, jwtSecret);
     const { sub: userId } = decoded;
+    const user = await mongoose.model('User').findById(userId);
 
-    try {
-      const user = await mongoose.model('User').findById(userId);
-
-      if (!user) {
-        return unauthorized();
-      } else {
-        req.user = user;
-        next();
-      }
-    } catch (e) {
-      unauthorized();
+    if (!user) {
+      throw new Error('Unauthorized');
     }
-  });
+
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).end();
+  }
 };
 
 export default checkAuth;
